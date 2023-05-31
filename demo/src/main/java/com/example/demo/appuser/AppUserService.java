450cbed3e2fbec6440331b5f18cfab96a10feaaf
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2023-05-31 21:32:56 +1000

    Service class - method = finds the user with given email

---
+++ demo/src/main/java/com/example/demo/appuser/AppUserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor //boilerplate for constructor
+public class AppUserService implements UserDetailsService {
+
+    private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
+    private final AppUserRepository appUserRepository;
+
+    @Override //Enables us to find user once we try to log in
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
+    }
+}
